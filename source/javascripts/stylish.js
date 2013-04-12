d26bbe2218d256a6ecd7260ef17bae6bc99cca76
Hondo <hondo@cantina.co>
2013-04-12 13:15:12 -0400

    Add fonts

--- source/javascripts/stylish.js
+++ source/javascripts/stylish.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
-	$('.stylish-toggle').on('click', function () {
+	$('.stylish-toggle').on('touchend click', function (ev) {
+		ev.preventDefault()
+		ev.stopPropagation()
+		
 		$('.stylish-container').toggleClass('stylish-active')
 	})
 
